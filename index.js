7f6f34b35c63bfec24277f72c07d5b34220d4fca
Hafiz Furqan Ahmad <hfurqan.ai@gmail.com>
2023-11-10 15:38:34 +0500

    theme changed

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
-  $('.slider').slick({
-    dots: false,
-    arrow: false,
-    infinite: true,
-    arrow: false,
-    speed: 400,
-    adaptiveHeight: true,
-    autoplay: true,
-    arrows: false,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
   $('#loader').css('display', 'none');
 });
+$('#themeChange').on('click', (e) => {
+  $('html').toggleClass('dark');
+})
